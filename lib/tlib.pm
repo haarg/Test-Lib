108c51627b15e911a52a8c2dc219d3c663900568
Graham Knop <haarg@haarg.org>
2012-03-20 15:10:32 -0400

    fix search order, load lib, exit sub properly

--- lib/tlib.pm
+++ lib/tlib.pm
@@ -2,6 +2,7 @@ package tlib;
 use strict;
 use File::Spec;
 use Cwd ();
+use lib ();
 
 sub import {
     my $class = shift;
@@ -10,8 +11,7 @@ sub import {
         my $file = File::Spec->rel2abs((caller)[1]);
         $dir = File::Spec->catpath((File::Spec->splitpath($file))[0,1], '');
     }
-    my $i = 5;
-    while ($i--) {
+    for my $i (0..5) {
         my $tdir = File::Spec->catdir($dir, (File::Spec->updir) x $i);
         my $abs_path = Cwd::abs_path($tdir);
         my $dirname = (File::Spec->splitdir($abs_path))[-1];
@@ -20,7 +20,7 @@ sub import {
             my $tlib = File::Spec->catdir($tdir, 'lib');
             if (-d $tlib) {
                 lib->import($tlib);
-                last;
+                return;
             }
         }
     }
